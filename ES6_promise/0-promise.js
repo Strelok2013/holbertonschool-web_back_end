762487472bb824ee19413f99df306ae620eb2de1
Vlad <vgozhyi@gmail.com>
2024-12-13 12:27:03 +1100

    Accelerated-course ES6-Promises submission

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+export default function getResponseFromAPI() {
+    const myPromise = new Promise(() => {
+      setTimeout(() => {
+        // console.log('foo');
+      }, 300);
+    });
+  
+    return myPromise;
+  }
---
+++ ES6_promise/README.md
@@ -0,0 +1,1 @@
+Project directory for ES6 Promises.
